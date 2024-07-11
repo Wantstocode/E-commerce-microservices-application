8300b3e4d1d9346fd3291f0f145632b1391bbd7c
kiran <kirancb0702@gmail.com>
2024-07-11 16:16:22 +0530

    first commit

--- services/notification-server/src/main/java/com/kiran/notification_server/notification/NotificationRepository.java
+++
@@ -1,6 +0,0 @@
-package com.kiran.notification_server.notification;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface NotificationRepository extends MongoRepository<Notification, String> {
-}
--- services/order-server/src/main/java/com/kiran/order_server/orderline/OrderLineRequest.java
+++
@@ -1,9 +0,0 @@
-package com.kiran.order_server.orderline;
-
-public record OrderLineRequest(
-        Integer id,
-        Integer orderId,
-        Integer productId,
-        double quantity
-) {
-}
