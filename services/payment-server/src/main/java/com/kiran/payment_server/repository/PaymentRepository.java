2caa3cdb051eb246483c8c7079deb6fe0774bf77
kirancb6620 <120893466+Wantstocode@users.noreply.github.com>
2024-07-11 11:49:47 +0530

    Delete services directory

--- services/payment-server/src/main/java/com/kiran/payment_server/repository/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kiran.payment_server.repository;
-
-import com.kiran.payment_server.model.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Integer> {
-
-}
